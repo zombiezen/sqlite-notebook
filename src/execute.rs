686ed4486c22bf2588b4720df686c856ffb6b5aa
Ross Light <ross@zombiezen.com>
2023-08-29 14:19:38 -0700

    Support double-quoted strings in dot commands

--- src/execute.rs
+++ src/execute.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::fmt::{self, Write};
-use std::iter;
+use std::iter::{self, FusedIterator, Peekable};
 use std::mem;
 
 use anyhow::{anyhow, Result};
@@ -474,7 +474,11 @@ impl<'a> DotCommandLine<'a> {
                         Some(tail) => line = tail,
                     }
                 }
-                // TODO(soon): Double-quotes.
+                Some(b'"') => {
+                    let (arg, tail) = unquote_c_string(line)?;
+                    args.push(arg);
+                    line = tail;
+                }
                 _ => match line.find(char::is_whitespace) {
                     None => {
                         args.push(line.into());
@@ -487,6 +491,58 @@ impl<'a> DotCommandLine<'a> {
                     }
                 },
             }
+        }
+    }
+}
+
+fn unquote_c_string<'a>(s: &'a str) -> Result<(Cow<'a, str>, &'a str)> {
+    let s = s.strip_prefix('"').expect("C string must start with '\"'");
+
+    let (arg, tail) = cut(s, "\"");
+    if arg.find('\\').is_none() {
+        return Ok((arg.into(), tail.unwrap_or("")));
+    }
+    let mut chars = s.chars();
+    let mut iter = chars.by_ref().peekable();
+    let mut arg = String::new();
+    loop {
+        match iter.next() {
+            None => return Ok((arg.into(), "")),
+            Some('"') => return Ok((arg.into(), chars.as_str())),
+            Some('\\') => match iter.next() {
+                None | Some('\\') => arg.push('\\'),
+                Some('a') => arg.push('\x07'),
+                Some('b') => arg.push('\x08'),
+                Some('t') => arg.push('\t'),
+                Some('n') => arg.push('\n'),
+                Some('v') => arg.push('\x0b'),
+                Some('f') => arg.push('\x0c'),
+                Some('r') => arg.push('\r'),
+                Some('x') => {
+                    let mut hex_val = 0u8;
+                    for digit in take_while_map(2, &mut iter, |c| c.to_digit(16)) {
+                        hex_val = (hex_val << 4) | (digit as u8);
+                    }
+                    let c: char = hex_val.into();
+                    if !c.is_ascii() {
+                        return Err(anyhow!("parse dot command: non-ASCII hex escape found"));
+                    }
+                    arg.push(c);
+                }
+                Some(c @ '0'..='7') => {
+                    let mut oct_val = c.to_digit(8).unwrap() as u8;
+                    for digit in take_while_map(2, &mut iter, |c| c.to_digit(8)) {
+                        oct_val = (oct_val << 3) | (digit as u8);
+                    }
+                    let c: char = oct_val.into();
+                    if !c.is_ascii() {
+                        return Err(anyhow!("parse dot command: non-ASCII octal escape found"));
+                    }
+                    arg.push(c);
+                }
+                Some(c) => arg.push(c),
+            },
+            Some(c) => arg.push(c),
         }
     }
 }
@@ -535,6 +591,74 @@ impl<'a> fmt::Display for EscapeHtml<'a> {
     }
 }
 
+/// The implementation of [`take_while_map`].
+struct TakeWhileMap<'a, I: Iterator, F> {
+    iter: &'a mut Peekable<I>,
+    filter_map: F,
+    take: usize,
+}
+
+/// Returns an iterator that takes at most n items
+/// from an underlying `Peekable` iterator,
+/// stopping once the `filter_map` function returns `None`.
+fn take_while_map<'a, I, F, T>(
+    n: usize,
+    iter: &'a mut Peekable<I>,
+    filter_map: F,
+) -> TakeWhileMap<'a, I, F>
+where
+    I: Iterator,
+    F: FnMut(&<I as Iterator>::Item) -> Option<T>,
+{
+    TakeWhileMap {
+        iter,
+        filter_map,
+        take: n,
+    }
+}
+
+impl<'a, T, I, F> Iterator for TakeWhileMap<'a, I, F>
+where
+    I: Iterator,
+    F: FnMut(&<I as Iterator>::Item) -> Option<T>,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.take == 0 {
+            return None;
+        }
+        match self.iter.peek() {
+            Some(x) => match (self.filter_map)(x) {
+                Some(y) => {
+                    debug_assert!(self.iter.next().is_some());
+                    self.take -= 1;
+                    Some(y)
+                }
+                None => {
+                    self.take = 0;
+                    None
+                }
+            },
+            None => {
+                self.take = 0;
+                None
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.take))
+    }
+}
+
+impl<'a, T, I, F> FusedIterator for TakeWhileMap<'a, I, F>
+where
+    I: Iterator,
+    F: FnMut(&<I as Iterator>::Item) -> Option<T>,
+{
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -565,6 +689,48 @@ mod tests {
             DotCommandLine {
                 name: "foo",
                 args: vec!["1 2".into(), "3".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"1 2\" 3").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["1 2".into(), "3".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"1\\n2\" 3").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["1\n2".into(), "3".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"1\\\"2\" 3").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["1\"2".into(), "3".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"\\x").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["\x00".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"\\x123").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["\x123".into()],
+            }
+        );
+        assert_eq!(
+            DotCommandLine::from_str(".foo \"\\1237").unwrap(),
+            DotCommandLine {
+                name: "foo",
+                args: vec!["\x537".into()],
             }
         );
     }
